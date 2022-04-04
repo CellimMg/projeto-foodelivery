3725bafc0a196122ca9055283f65be1e9ccfe93a
Marcelo <cello.ma16@gmail.com>
2022-04-04 17:30:25 -0300

    finalizando a manipulacao de DOM com js

--- script.js
+++ script.js
@@ -1,39 +1,185 @@
-let prato, bebida, sobremesa, nome, endereco;
+let prato, sobremesa, bebida, pratoNome, bebidaNome, sobremesaNome, pratoPreco, bebidaPreco, sobremesaPreco, nome, endereco;
 
-function selectPrato(cardPrato) {
+function selectPratoFrango(cardPrato) {
     if (prato !== cardPrato) {
         if (prato != null) {
+            let divCheck = prato.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
             prato.classList.remove("selected");
         }
         prato = cardPrato;
         prato.classList.add("selected");
     }
 
-    console.log(prato.innerHTML);
+    let divCheck = prato.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+
+    pratoNome = "Frango Yin Yang";
+    pratoPreco = 14.90;
 
     checkFinish();
 }
 
-function selectBebida(cardBebida) {
+function selectPratoPeixe(cardPrato) {
+    if (prato !== cardPrato) {
+        if (prato != null) {
+            let divCheck = prato.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
+            prato.classList.remove("selected");
+        }
+        prato = cardPrato;
+        prato.classList.add("selected");
+    }
+
+    let divCheck = prato.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+
+    pratoNome = "Filé de Peixe";
+    pratoPreco = 19.90;
+
+    checkFinish();
+}
+
+function selectPratoLasanha(cardPrato) {
+    if (prato !== cardPrato) {
+        if (prato != null) {
+            let divCheck = prato.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
+            prato.classList.remove("selected");
+        }
+        prato = cardPrato;
+        prato.classList.add("selected");
+    }
+
+    let divCheck = prato.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+
+    pratoNome = "Lasanha";
+    pratoPreco = 16.90;
+
+    checkFinish();
+}
+
+
+function selectBebidaCoca(cardBebida) {
     if (bebida !== cardBebida) {
         if (bebida != null) {
+            let divCheck = bebida.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
             bebida.classList.remove("selected");
         }
         bebida = cardBebida;
         bebida.classList.add("selected");
     }
+
+    let divCheck = bebida.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+    bebidaNome = "Coca-Cola";
+    bebidaPreco = 6.90;
+
     checkFinish();
 }
 
-function selectSobremesa(cardSobremesa) {
+function selectBebidaFanta(cardBebida) {
+    if (bebida !== cardBebida) {
+        if (bebida != null) {
+            let divCheck = bebida.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
+            bebida.classList.remove("selected");
+        }
+        bebida = cardBebida;
+        bebida.classList.add("selected");
+    }
+
+    let divCheck = bebida.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+
+    bebidaNome = "Fanta Laranja";
+    bebidaPreco = 5.90;
+
+    checkFinish();
+}
+
+function selectBebidaSuco(cardBebida) {
+    if (bebida !== cardBebida) {
+        if (bebida != null) {
+            let divCheck = bebida.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
+            bebida.classList.remove("selected");
+        }
+        bebida = cardBebida;
+        bebida.classList.add("selected");
+    }
+
+    let divCheck = bebida.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+
+    bebidaNome = "Suco de Melancia";
+    bebidaPreco = 8.90;
+
+    checkFinish();
+}
+
+function selectSobremesaPudim(cardSobremesa) {
     const element = cardSobremesa;
     if (sobremesa !== cardSobremesa) {
         if (sobremesa != null) {
+            let divCheck = sobremesa.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
             sobremesa.classList.remove("selected");
         }
         sobremesa = cardSobremesa;
         sobremesa.classList.add("selected");
     }
+
+    let divCheck = sobremesa.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+
+    sobremesaNome = "Pudim";
+    sobremesaPreco = 7.90;
+
+    checkFinish();
+}
+
+function selectSobremesaSorvete(cardSobremesa) {
+    const element = cardSobremesa;
+    if (sobremesa !== cardSobremesa) {
+        if (sobremesa != null) {
+            let divCheck = sobremesa.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
+            sobremesa.classList.remove("selected");
+        }
+        sobremesa = cardSobremesa;
+        sobremesa.classList.add("selected");
+    }
+
+    let divCheck = sobremesa.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+
+    sobremesaNome = "Sorvete";
+    sobremesaPreco = 9.90;
+
+    checkFinish();
+}
+
+function selectSobremesaPetit(cardSobremesa) {
+    const element = cardSobremesa;
+    if (sobremesa !== cardSobremesa) {
+        if (sobremesa != null) {
+            let divCheck = sobremesa.querySelector('div');
+            divCheck.querySelector('.check img').style.display = 'none';
+            sobremesa.classList.remove("selected");
+        }
+        sobremesa = cardSobremesa;
+        sobremesa.classList.add("selected");
+    }
+
+    let divCheck = sobremesa.querySelector('div');
+    divCheck.querySelector('.check img').style.display = 'block';
+
+    sobremesaNome = "Petit Gateau";
+    sobremesaPreco = 14.90;
+
     checkFinish();
 }
 
@@ -55,7 +201,7 @@ function finish() {
         let uri = getUri(mensagem);
 
         alert(mensagem);
-        //window.open(uri);
+        window.open(uri);
     }
 }
 
@@ -65,28 +211,24 @@ function solicitarDados() {
 }
 
 function createMessage() {
-
     let nomePrato, nomeSobremesa, nomeBebida, total;
-
     total = getTotal();
-
     let mensagem = "Olá, gostaria de fazer o pedido\n" +
-        `- Prato: ${prato}\n` +
-        `- Bebida: ${bebida}\n` +
-        `- Sobremesa: ${sobremesa}\n` +
-        `Total: R$ ${total}\n\n` +
+        `- Prato: ${pratoNome}\n` +
+        `- Bebida: ${bebidaNome}\n` +
+        `- Sobremesa: ${sobremesaNome}\n` +
+        `Total: R$ ${getTotal().toFixed(2)}\n\n` +
         `Nome: ${nome}\n` +
         `Endereço: ${endereco}\n`;
     return mensagem;
 }
 
 function getUri(mensagem) {
-    let uri = encodeURI("https://wa.me/+5538991836830?text=" + mensagem);
+    let uri = "https://wa.me/+5538991836830?text=" + encodeURIComponent(mensagem);
     return uri;
 }
 
 
 function getTotal() {
-    let valorPrato, valorSobremesa, valorBebida;
-    return valorPrato + valorSobremesa + valorBebida;
+    return pratoPreco + sobremesaPreco + bebidaPreco;
 }
